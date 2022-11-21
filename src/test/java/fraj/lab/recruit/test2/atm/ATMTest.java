707f70ab5061c83161482f2d5fa6fcbb7fb05023
Ali Amanzegouarene <amanzegouareneali@gmail.com>
2022-11-21 15:09:19 -0500

    Unit tests with full coverage and specification respectful

--- src/test/java/fraj/lab/recruit/test2/atm/ATMTest.java
+++ src/test/java/fraj/lab/recruit/test2/atm/ATMTest.java
@@ -1,5 +1,100 @@
 package fraj.lab.recruit.test2.atm;
 
-public class ATMTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+class ATMTest {
+
+    private ATM atm;
+
+    private AmountSelector amountSelectorMock;
+
+    private CashManager cashManagerMock;
+
+    private PaymentProcessor paymentProcessorMock;
+
+    @BeforeEach
+    public void beforeAll() {
+        amountSelectorMock = mock(AmountSelector.class);
+        cashManagerMock = mock(CashManager.class);
+        paymentProcessorMock = mock(PaymentProcessor.class);
+
+        atm = new ATM(amountSelectorMock, cashManagerMock, paymentProcessorMock);
+    }
+
+    @Test
+    void givenNegativeSelectedAmount_whenRunCashWithdrawal_shouldThrownATMTechnicalExceptionAndStopProcessing() {
+        // Given
+        when(amountSelectorMock.selectAmount()).thenReturn(-1);
+
+        assertThatThrownBy(
+                // When
+                () -> atm.runCashWithdrawal())
+                // Then
+                .isInstanceOf(ATMTechnicalException.class);
+        verifyNoInteractions(paymentProcessorMock);
+        verifyNoInteractions(cashManagerMock);
+
+    }
+
+    @Test
+    void givenZeroSelectedAmount_whenRunCashWithdrawal_shouldThrownATMTechnicalExceptionAndStopProcessing() {
+        // Given
+        when(amountSelectorMock.selectAmount()).thenReturn(0);
+
+        assertThatThrownBy(
+                // When
+                () -> atm.runCashWithdrawal())
+                // Then
+                .isInstanceOf(ATMTechnicalException.class);
+    }
+
+    @Test
+    void givenNotAvailableSelectedAmount_whenRunCashWithdrawal_shouldReturnCashNotAvailableAndDontProcessPayment() throws ATMTechnicalException {
+        // Given
+        when(amountSelectorMock.selectAmount()).thenReturn(100);
+        when(cashManagerMock.canDeliver(100)).thenReturn(false);
+
+        // When
+        ATMStatus atmStatus = atm.runCashWithdrawal();
+
+        // Then
+        assertThat(atmStatus).isEqualByComparingTo(ATMStatus.CASH_NOT_AVAILABLE);
+        verifyNoInteractions(paymentProcessorMock);
+        verify(cashManagerMock, times(0)).deliver(anyInt());
+    }
+
+    @Test
+    void givenAvailableSelectedAmountButRejectedPayment_whenRunCashWithdrawal_shouldReturnPaymentRejectedAndDontDeliverCash() throws ATMTechnicalException {
+        // Given
+        when(amountSelectorMock.selectAmount()).thenReturn(100);
+        when(cashManagerMock.canDeliver(100)).thenReturn(true);
+        when(paymentProcessorMock.pay(100)).thenReturn(PaymentStatus.FAILURE);
+
+        // When
+        ATMStatus atmStatus = atm.runCashWithdrawal();
+
+        // Then
+        assertThat(atmStatus).isEqualByComparingTo(ATMStatus.PAYMENT_REJECTED);
+        verify(cashManagerMock, times(0)).deliver(anyInt());
+    }
+
+    @Test
+    void givenAvailableSelectedAmountAndSuccessPayment_whenRunCashWithdrawal_shouldReturnDoneAndDeliverCash() throws ATMTechnicalException {
+        // Given
+        when(amountSelectorMock.selectAmount()).thenReturn(100);
+        when(cashManagerMock.canDeliver(100)).thenReturn(true);
+        when(paymentProcessorMock.pay(100)).thenReturn(PaymentStatus.SUCCESS);
+
+        // When
+        ATMStatus atmStatus = atm.runCashWithdrawal();
+
+        // Then
+        assertThat(atmStatus).isEqualByComparingTo(ATMStatus.DONE);
+        verify(cashManagerMock, times(1)).deliver(100);
+    }
 }
